839dd913b794030421ec313d4cbc19f2d6e62d47
KolesnikVV <Voltpluc@ya.ru>
2023-02-12 12:52:52 +0300

    task1

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+    public static void task1 (){
+        System.out.println("Задание 1");
+        byte a = -35;
+        System.out.println("Значение переменной a с типом byte равно " + a);
+        short b = 222;
+        System.out.println("Значение переменной b с типом short равно " + b);
+        int c = 40588;
+        System.out.println("Значение переменной c с типом int равно " + c);
+        long d = 2548967258L;
+        System.out.println("Значение переменной d с типом long равно " + d );
+        float e = 5.55f;
+        System.out.println("Значение переменной e с типом float равно " + e);
+        double f = 12.36;
+        System.out.println("Значение переменной f с типом double равно " + f);
+
+    }
+    public static void task2(){
+        System.out.println("Задание 2");
+        double a = 27.12;
+        System.out.println("число " + a +" относится к типу переменных double");
+        long b = 987678965549L;
+        System.out.println("число " + b +" относится к типу переменной long");
+        float c = 2.786f;
+        System.out.println("число " + c + " относится к типу переменной float");
+        short d = 569;
+        short e = -159;
+        short f = 27897;
+        System.out.println("числа " + d +"," + e + "," + f + "," + " относится к типу переменной short");
+        byte j = 67;
+        System.out.println("число " + j + " относится к типу переменной byte");
+
+    }
+
+}
